88ba52fcc89777c1fd1088f3cd1703f5cf50e21b
satish <satkumar12@publicisgroupe.net>
2025-09-14 21:46:19 +0530

    ThreeSumClosest

--- src/com/practice/leetcode/arrays/ThreeSumClosest.java
+++ src/com/practice/leetcode/arrays/ThreeSumClosest.java
@@ -1,5 +1,38 @@
 package com.practice.leetcode.arrays;
 
+import java.util.Arrays;
+
 public class ThreeSumClosest {
+	public int threeSumClosest(int[] nums, int target) {
+
+		int closestSum = 100000;
+
+		Arrays.sort(nums);
+
+		for (int i = 0; i < nums.length - 2; i++) {
+
+			int l = i + 1;
+			int r = nums.length - 1;
+
+			while (l < r) {
+				int sum = nums[i] + nums[l] + nums[r];
+
+				if (Math.abs(target - sum) < Math.abs(target - closestSum)) {
+					closestSum = sum;
+				}
+
+				if (sum < target) {
+					l++;
+				} else if (sum > target) {
+					r--;
+				} else if (sum == target) {
+					return sum;
+				}
+			}
+		}
+
+		return closestSum;
+
+	}
 
 }
