e49141d34cba6997410c5800e4813a312ff71ae7
satish <satkumar12@publicisgroupe.net>
2025-09-14 20:04:19 +0530

    Remote duplicates from sorted array

---
+++ src/com/practice/leetcode/arrays/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,20 @@
+package com.practice.leetcode.arrays;
+
+public class RemoveDuplicatesFromSortedArray {
+	public int removeDuplicates(int[] nums) {
+        int k = 1;
+
+        for (int i=1;i<nums.length;i++) {
+
+            if (nums[k-1] != nums[i]) {
+                nums[k] = nums[i];
+                k++;
+            } else {
+                continue;
+            }
+        }
+
+        return k;
+    }
+
+}
