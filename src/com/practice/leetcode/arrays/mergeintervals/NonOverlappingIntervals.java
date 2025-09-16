80443b3496ea45a1baaeb49b8190ff5aa6d18be9
satish <satkumar12@publicisgroupe.net>
2025-09-16 13:37:33 +0530

    Intervals

--- src/com/practice/leetcode/arrays/mergeintervals/NonOverlappingIntervals.java
+++ src/com/practice/leetcode/arrays/mergeintervals/NonOverlappingIntervals.java
@@ -1,5 +1,26 @@
 package com.practice.leetcode.arrays.mergeintervals;
 
+import java.util.Arrays;
+
 public class NonOverlappingIntervals {
+	public int eraseOverlapIntervals(int[][] intervals) {
+
+        Arrays.sort(intervals, (o1, o2) -> Integer.compare(o1[1], o2[1]));
+
+        int[] previous = intervals[0];
+        int count = 0;
+
+        for (int i = 1; i < intervals.length; i++) {
+            if (previous[1] <= intervals[i][0]) {
+                //non-overlapping
+                previous = intervals[i];
+            } else {
+                count++;
+            }
+        }
+
+        return count;
+
+    }
 
 }
