7cf7f087f963c7b73f6c9c900cefda12501b074c
satish <satkumar12@publicisgroupe.net>
2025-09-19 22:01:15 +0530

    PrefixSum

--- src/com/practice/leetcode/arrays/prefixSum/FindAllDuplicatesInAnArray.java
+++ src/com/practice/leetcode/arrays/prefixSum/FindAllDuplicatesInAnArray.java
@@ -1,5 +1,29 @@
 package com.practice.leetcode.arrays.prefixSum;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FindAllDuplicatesInAnArray {
+
+	public List<Integer> findDuplicates(int[] nums) {
+
+		List<Integer> list = new ArrayList<>();
+
+		if (nums.length == 1) {
+			return list;
+		}
+
+		for (int num : nums) {
+			int idx = Math.abs(num) - 1;
+			if (nums[idx] < 0) {
+				list.add(Math.abs(num));
+			} else {
+				nums[idx] = -nums[idx];
+			}
+		}
+
+		return list;
+
+	}
 
 }
--- src/com/practice/leetcode/arrays/prefixSum/SubArrayProductLessThanK.java
+++ src/com/practice/leetcode/arrays/prefixSum/SubArrayProductLessThanK.java
@@ -1,32 +1,32 @@
 package com.practice.leetcode.arrays.prefixSum;
 
 public class SubArrayProductLessThanK {
-	
+
 	public int numSubarrayProductLessThanK(int[] nums, int k) {
 
-        if (k<=1) {
-            return 0;
-        }
+		if (k <= 1) {
+			return 0;
+		}
 
-        int product =1;
+		int product = 1;
 
-        int subArrayCount = 0;
+		int subArrayCount = 0;
 
-        int l=0;
-        int r=0;
-        for (r=0;r<nums.length;r++) {
-            product = product*nums[r];
+		int l = 0;
+		int r = 0;
+		for (r = 0; r < nums.length; r++) {
+			product = product * nums[r];
 
-            while (product >= k) {
-                product/=nums[l];
-                l++;
-            }
+			while (product >= k) {
+				product /= nums[l];
+				l++;
+			}
 
-            subArrayCount+=r-l+1;
-        }
+			subArrayCount += r - l + 1;
+		}
 
-        return subArrayCount;
-        
-    }
+		return subArrayCount;
+
+	}
 
 }
