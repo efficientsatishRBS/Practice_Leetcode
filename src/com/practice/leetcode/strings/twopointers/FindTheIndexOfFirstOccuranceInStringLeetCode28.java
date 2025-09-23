d42220099faf442530a624105f1296d408bbf849
satish <satkumar12@publicisgroupe.net>
2025-09-23 16:06:23 +0530

    Leetcode_28

---
+++ src/com/practice/leetcode/strings/twopointers/FindTheIndexOfFirstOccuranceInStringLeetCode28.java
@@ -0,0 +1,27 @@
+package com.practice.leetcode.strings.twopointers;
+
+public class FindTheIndexOfFirstOccuranceInStringLeetCode28 {
+
+	public int strStr(String haystack, String needle) {
+		int hLen = haystack.length();
+		int nLen = needle.length();
+
+		int nIndex = 0;
+
+		for (int i = 0; i < hLen; i++) {
+			if (haystack.charAt(i) == needle.charAt(nIndex)) {
+				nIndex++;
+			} else {
+				i = i - nIndex;
+				nIndex = 0;
+			}
+
+			if (nIndex == nLen) {
+				return i - nLen + 1;
+			}
+		}
+
+		return -1;
+	}
+
+}
