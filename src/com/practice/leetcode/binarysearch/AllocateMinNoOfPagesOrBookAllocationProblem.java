7d3d9e8349f06347c573280fe939ac01425517bd
satish <satkumar12@publicisgroupe.net>
2025-10-02 11:23:49 +0530

    BookAllocationProblem

---
+++ src/com/practice/leetcode/binarysearch/AllocateMinNoOfPagesOrBookAllocationProblem.java
@@ -0,0 +1,49 @@
+package com.practice.leetcode.binarysearch;
+
+public class AllocateMinNoOfPagesOrBookAllocationProblem {
+
+	public int findPages(int[] arr, int k) {
+
+		if (k > arr.length) {
+			return -1;
+		}
+
+		int minPages = 0;
+		int maxPages = 0;
+
+		for (int pages : arr) {
+			minPages = Math.max(minPages, pages);
+			maxPages += pages;
+		}
+
+		while (minPages < maxPages) {
+
+			int mid = minPages + (maxPages - minPages) / 2;
+
+			if (isPossible(arr, k, mid)) {
+				maxPages = mid;
+			} else {
+				minPages = mid + 1;
+			}
+		}
+
+		return minPages;
+
+	}
+
+	private boolean isPossible(int[] arr, int students, int limit) {
+		int studentCount = 1;
+		int pagesCount = 0;
+
+		for (int pages : arr) {
+			if (pagesCount + pages > limit) {
+				pagesCount = 0;
+				studentCount++;
+			}
+			pagesCount += pages;
+		}
+
+		return studentCount <= students;
+	}
+
+}
